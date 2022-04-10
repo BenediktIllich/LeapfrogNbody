7a1d92205f52d41cda04e8f295e99b86dc85e430
Benedikt Illich <benedikt.illich@protonmail.com>
2022-04-10 18:40:22 +0200

    Add files via upload
    introduced parallel computing of the acceleration at each timestep

--- leapfrog.py
+++ leapfrog.py
@@ -3,12 +3,11 @@ import matplotlib.pyplot as plt
 import math
 import scipy.integrate as integrate
 import random
+import multiprocessing
 from IPython.display import clear_output
 import time
-from numba import jit
 
 
-start = time.time()
 
 # constants in pc-M_sol-Ma units
 G = 0.004491
@@ -29,30 +28,53 @@ def grav_acc(m_j, d_ij, softening = 0):
     return G * m_j * d_ij / ((absolute(d_ij) + softening)**3)
 
 
-def acc_tensor(positions, masses):
-    """Finds the acceleration tensor given the positions and masses."""
-    a = np.zeros((3, n, n))
+def distribute(n,p):
+    """Finds out which processor needs to compute which section of the particle population"""
+    base, extra = divmod(n,p)
+    distribution = np.array([base + (i < extra) for i in range(p)])
+    distribution = np.concatenate([np.array([0]), distribution])
+    distribution = np.concatenate([distribution, np.array([0])])
+    return distribution.flatten()
+    
+    
+def single_thread(i, n, masses, positions, softening=0):
+    """This version of single thread just lets the starmap do more of the work"""
+        
+    accelerations_on_i = np.zeros((3,n)) # stores all the contributions to the acc on i
+    for j in range(i):
+        # j-loop 1: get half of the accelerations from the j particle on the i particle
+        d_ij = distance(positions[:,i], positions[:,j])
+        accelerations_on_i[:,j] = grav_acc(masses[j], d_ij, softening)
+    for j in range(i+1, n):
+        # j-loop 2: get the other half to exclude i==j
+        d_ij = distance(positions[:,i], positions[:,j])
+        accelerations_on_i[:,j] = grav_acc(masses[j], d_ij, softening)
+
+    total_acceleration_on_i = np.sum(accelerations_on_i, axis=1)
+    return total_acceleration_on_i
+    
+
+def pooled_acceleration(n, p, masses, positions, softening = 0):
+    """Pools the full particle population to the individual CPUs"""
+    input_array = np.empty(n, dtype=object) # array that stores the arguments passed to single thread
+    
     for i in range(n):
-        for j in range(n):
-            if i == j:
-                a[:, i, j] = 0
-            else:
-                d_ij = distance(positions[:, i], positions[:, j])
-                a[:, i, j] = grav_acc(masses[j], d_ij, softening) #acceleration on particle i from particle j
-    return a
-    
-    
-def cumulative_acc(positions, masses, i):
-    """finds the total acceleration on particle i.(inefficient for in the loop!)"""
-    a = np.zeros(3)
-    for j in range(0, n):    #acceleration from particle j
-        if j == i:
-            a = a
-        else:
-            d_ij = distance(positions[:, i], positions[:, j])
-            #print(d_ij)
-            a = a + grav_acc(masses[j], d_ij)
-    return a
+        # every i in this array is for one particle
+        input_array_i = np.empty(5, dtype=object)
+        input_array_i[0] = i # ID of the particle
+        input_array_i[1] = n # total number of particles scalar
+        input_array_i[2] = masses # array of all particle masses
+        input_array_i[3] = positions # array of positions
+        input_array_i[4] = softening # softening scalar
+        
+        input_array[i] = input_array_i
+
+
+    with multiprocessing.Pool(p) as pool:
+        accelerations_array = pool.starmap(single_thread, input_array)
+        pool.close()
+
+    return np.transpose(accelerations_array)
     
     
 def get_epsilon(kick_velocity):
@@ -66,12 +88,14 @@ def new_mass(epsilon):
     
     
 def random_direction():
+    """Generates a random direction in spherical coordinates"""
     theta = np.random.uniform(low = 0.0, high = np.pi)
     phi = np.random.uniform(low = 0.0, high = 2 * np.pi)
     return theta, phi
     
 
 def relative_cart_kick(kick_velocity):
+    """Generates a velocity Kick in a random direction and outputs it in cartesian coordinates"""
     theta, phi = random_direction()
     x_velocity = kick_velocity * np.cos(phi) * np.sin(theta)
     y_velocity = kick_velocity * np.sin(phi) * np.sin(theta)
@@ -96,81 +120,78 @@ def decay(decay_rate, kick_velocity, mass_array, velocity_array, time_step):
             
 
 
+if __name__ == "__main__":
 
-
-# number of particles
-n = 100
-# number of timesteps
-T = 1000
-# length of one time step (units!)
-t = 1 #Ma
-# accelerations
-accelerations = np.zeros((3, n))
-# masses
-M = 1e15
-masses = np.loadtxt(f'Initial_Setup/initial_masses_{n}_particles_M_{M}.txt')
-#masses = np.ones(n)
-# softening parameter (units!)
-softening = 1e4 #pc
-
-# decay parameters
-Gamma = 0 #Ma
-v_kick = 100 #pc/Ma
-
-# initial setup of the system (units!):
-# all txt files contain (3, n)-arrays where n gives the number of particles
-# positions
-positions = np.loadtxt(f'Initial_Setup/sphere_distribution_{n}_particles.txt')
-# velocities
-velocities = np.loadtxt(f'Initial_Setup/stationary_{n}_particles.txt')
-
-# file templates
-pos_template = 'Positions/k{i}positions.txt'
-vel_template = 'Velocities/k{i}velocities.txt'
-acc_template = 'Accelerations/k{i}accelerations.txt'
-# 0-step velocities and positions
-np.savetxt(vel_template.format(i=0), velocities)
-np.savetxt(pos_template.format(i=0), positions)
-
-
-# 0-step accelerations:
-#for i in range(n):
-#    #print(accelerations)
-#    accelerations[:, i] = cumulative_acc(positions, masses, i)
-#np.savetxt(acc_template.format(i=0), accelerations)
-
-a = acc_tensor(positions, masses)
-accelerations = np.sum(a, axis = 2)
-
-# outer loop for every time step:
-for k in range(1, T):
-    # update the position for all particles
-    positions += t * velocities + .5 * t**2 * accelerations
-    # save the new positions array
-    np.savetxt(pos_template.format(i=k), positions)
+    start = time.time()
     
-    #remember the last acceleration
-    accelerations_prior = accelerations
+    # number of particles
+    n = 2000
+    # number of processors
+    p = 4
+    # number of timesteps
+    T = 10
+    # length of one time step (units!)
+    t = 1 #Ma
+    # accelerations
+    accelerations = np.zeros((3, n))
+    # masses
+    M = 1e15
+    masses = np.loadtxt(f'Initial_Setup/initial_masses_{n}_particles_M_{M}.txt')
+    #masses = np.ones(n)
+    # softening parameter (units!)
+    softening = 1e4 #pc
     
-    # accelerations using acceleration tensor
-    a = acc_tensor(positions, masses)
-    accelerations = np.sum(a, axis = 2)
+    # decay parameters
+    Gamma = 0 #Ma
+    v_kick = 100 #pc/Ma
     
-    # save the new accelerations array
-    np.savetxt(acc_template.format(i=k), accelerations)
+    # initial setup of the system (units!):
+    # all txt files contain (3, n)-arrays where n gives the number of particles
+    # positions
+    positions = np.loadtxt(f'Initial_Setup/sphere_distribution_{n}_particles.txt')
+    # velocities
+    velocities = np.loadtxt(f'Initial_Setup/stationary_{n}_particles.txt')
     
-    # update the velocities for all particles
-    velocities += .5 * t * (accelerations_prior + accelerations)
-    # save the new velocities array
-    np.savetxt(vel_template.format(i=k), velocities)
-
-    # decay process
-    masses, velocities = decay(Gamma, v_kick, masses, velocities, t)
     
-    # progress update
-    clear_output(wait=True)
-    print(f'progress: {round((k+1)/(T) * 100, 3)}%')
-
-end = time.time()
-
-print(f'Simulation finished. It took {end - start} seconds')
+    # 0-step velocities and positions
+    np.savetxt(f'Velocities/k0velocities.txt', velocities)
+    np.savetxt(f'Positions/k0positions.txt', positions)
+    
+    
+    # 0-step accelerations:
+    
+    accelerations = pooled_acceleration(n, p, masses, positions, softening)
+    np.savetxt(f'Accelerations/k0accelerations.txt', accelerations)
+    
+    # outer loop for every time step:
+    for k in range(1, T):
+        # update the position for all particles
+        positions += t * velocities + .5 * t**2 * accelerations
+        # save the new positions array
+        np.savetxt(f'Positions/k{k}positions.txt', positions)
+    
+        #remember the last acceleration
+        accelerations_prior = accelerations
+    
+        # accelerations using pooled workers
+        accelerations = pooled_acceleration(n, p, masses, positions, softening)
+    
+        # save the new accelerations array
+        np.savetxt(f'Accelerations/k{k}accelerations.txt', accelerations)
+    
+        # update the velocities for all particles
+        velocities += .5 * t * (accelerations_prior + accelerations)
+        # save the new velocities array
+        np.savetxt(f'Velocities/k{k}velocities.txt', velocities)
+    
+        # decay process
+        masses, velocities = decay(Gamma, v_kick, masses, velocities, t)
+    
+        # progress update
+        clear_output(wait=True)
+        print(f'progress: {round((k+1)/(T) * 100, 3)}%')
+    
+    end = time.time()
+    
+    print(f'Simulation finished. It took {end - start} seconds')
+    
